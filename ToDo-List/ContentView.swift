cd36af88a2f70e372a0c6fbfaa52ff36eb593bb9
CHOIY3SUNG <choiyesung2848@naver.com>
2022-06-15 19:03:16 +0900

    Initial Commit

---
+++ ToDo-List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo-List
+//
+//  Created by Y3SUNG on 2022/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDo-List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo-List
+//
+//  Created by Y3SUNG on 2022/06/15.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
